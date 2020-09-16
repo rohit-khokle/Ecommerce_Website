374c4688d1b4864ac99d9ac7b4a1fca2103283e7
rk-aed <59897405+rk-aed@users.noreply.github.com>
2020-09-16 18:43:15 -0400

    Spring-boot setup

---
+++ spring-boot-ecommerce/src/main/java/com/ecommerceapp/dao/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerceapp.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecommerceapp.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	
+	
+	
+	
+}
